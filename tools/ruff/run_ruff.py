3c3ffa610c2064cd1417fbdcbbfbfa7e34ca91d2
Patou01 <pbobbink@gmail.com>
2026-02-23 18:35:34 +0100

    Well this is yikes, but kinda works

--- tools/ruff/run_ruff.py
+++ tools/ruff/run_ruff.py
@@ -1,21 +1,13 @@
 import os
 import subprocess
 import sys
+from enum import Enum
 from pathlib import Path
 
-cwd = os.getcwd()
-os.chdir(Path("~/monopatoux").resolve())
-print(cwd)
+
+os.chdir(Path("/home/drooppi/monopatoux"))
 here = Path(os.path.dirname(__file__))
-print(f"here: {here}", file=sys.stderr)
 RUFF_PATH = (here / "../../../+ruff+ruff/ruff-x86_64-unknown-linux-gnu/ruff").resolve()
-
-print(f"[run_ruff.py] Ruff binary path: {RUFF_PATH}", file=sys.stderr)
-print(f"[run_ruff.py] Ruff binary exists: {os.path.exists(RUFF_PATH)}", file=sys.stderr)
-
-if not os.path.exists(RUFF_PATH):
-    sys.stderr.write(f"Ruff binary not found at {RUFF_PATH}\n")
-    sys.exit(1)
 
 args = [RUFF_PATH] + sys.argv[2:]
 result = subprocess.run(args)
